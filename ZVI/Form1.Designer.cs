9df2765a196bd279cd59abe90e0a6bf1d29412df
zeastiecs <61218170+zeastiecs@users.noreply.github.com>
2020-03-05 22:21:25 -0600

    DO NOT RUN THIS ON YOUR MAIN COMPUTER!

---
+++ ZVI/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ZVI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formSkin1 = new FlatUI.FormSkin();
+            this.flatButton1 = new FlatUI.FlatButton();
+            this.flatAlertBox1 = new FlatUI.FlatAlertBox();
+            this.formSkin1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formSkin1
+            // 
+            this.formSkin1.BackColor = System.Drawing.Color.White;
+            this.formSkin1.BaseColor = System.Drawing.Color.Black;
+            this.formSkin1.BorderColor = System.Drawing.Color.Black;
+            this.formSkin1.Controls.Add(this.flatAlertBox1);
+            this.formSkin1.Controls.Add(this.flatButton1);
+            this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSkin1.FlatColor = System.Drawing.Color.Purple;
+            this.formSkin1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.formSkin1.ForeColor = System.Drawing.Color.White;
+            this.formSkin1.HeaderColor = System.Drawing.Color.Purple;
+            this.formSkin1.HeaderMaximize = false;
+            this.formSkin1.Location = new System.Drawing.Point(0, 0);
+            this.formSkin1.Name = "formSkin1";
+            this.formSkin1.Size = new System.Drawing.Size(800, 450);
+            this.formSkin1.TabIndex = 0;
+            this.formSkin1.Text = "Zeasties Virus Remover";
+            this.formSkin1.Click += new System.EventHandler(this.formSkin1_Click);
+            // 
+            // flatButton1
+            // 
+            this.flatButton1.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton1.BaseColor = System.Drawing.Color.Purple;
+            this.flatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton1.Location = new System.Drawing.Point(196, 51);
+            this.flatButton1.Name = "flatButton1";
+            this.flatButton1.Rounded = false;
+            this.flatButton1.Size = new System.Drawing.Size(455, 32);
+            this.flatButton1.TabIndex = 0;
+            this.flatButton1.Text = "Delete";
+            this.flatButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton1.Click += new System.EventHandler(this.flatButton1_Click);
+            // 
+            // flatAlertBox1
+            // 
+            this.flatAlertBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.flatAlertBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatAlertBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.flatAlertBox1.kind = FlatUI.FlatAlertBox._Kind.Success;
+            this.flatAlertBox1.Location = new System.Drawing.Point(196, 3);
+            this.flatAlertBox1.Name = "flatAlertBox1";
+            this.flatAlertBox1.Size = new System.Drawing.Size(455, 42);
+            this.flatAlertBox1.TabIndex = 1;
+            this.flatAlertBox1.Text = "Threats have been found on your computer. Delete them now";
+            this.flatAlertBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.formSkin1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.formSkin1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatUI.FormSkin formSkin1;
+        private FlatUI.FlatButton flatButton1;
+        private FlatUI.FlatAlertBox flatAlertBox1;
+    }
+}
+
